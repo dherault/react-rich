c0c17e4cb2559c586a285ec0b87762079decad96
David Hérault <dherault@gmail.com>
2023-02-15 13:43:02 +0100

    v2

--- react-rich-demo/src/App.tsx
+++ react-rich-demo/src/App.tsx
@@ -1,25 +1,8 @@
 import { useState } from 'react'
-import RichTextEditor from 'react-rich'
+import RichTextEditor, { createEmptyState } from 'react-rich'
 
 function App() {
-  const [content, setContent] = useState([
-    {
-      id: '0',
-      type: 'image',
-      html: 'https://picsum.photos/200/300',
-    },
-    {
-      id: '1',
-      type: 'paragraph',
-      children: [
-        {
-          id: '11',
-          type: 'text',
-          html: 'Hello World!',
-        },
-      ],
-    },
-  ])
+  const [content, setContent] = useState(createEmptyState())
 
   return (
     <RichTextEditor
